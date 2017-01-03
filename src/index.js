5708cb46b7552bcad6a1eb4f6a48093361a4a9c2
marton bognar <martonbognar@sch.bme.hu>
2017-01-03 17:10:00 +0100

    Start working on calculator

--- src/index.js
+++ src/index.js
@@ -66,7 +66,43 @@ class Drink extends Component {
     return (
       <div className='drink'>
         <p>{this.state.name}: {this.state.amount} cl of {this.state.strength}% alcohol</p>
-        <p>Started drinking at {this.state.startTime}, finished at {this.state.finishTime}</p>
+        <p>Started drinking at {this.state.startTime.getTime()}, finished at {this.state.finishTime.getTime()}</p>
+      </div>
+    );
+  }
+}
+
+class Calculator extends Component {
+  constructor(props) {
+    super(props);
+
+    this.ebac = this.ebac.bind(this);
+    this.calculateEbac = this.calculateEbac.bind(this);
+  }
+
+  calculateEbac() {
+    let alcohol = 0;
+    let period = ((new Date()).getTime() - this.props.drinks[0].startTime.getTime()) / (1000 * 60 * 60);
+    for (let i = 0; i < this.props.drinks.length; i++) {
+      let alcoholml = (parseInt(this.props.drinks[i].amount, 10) / 10) * parseInt(this.props.drinks[i].strength, 10);
+      let grams = alcoholml * 0.789;
+      alcohol += grams;
+    }
+    return this.ebac(alcohol, period).toFixed(5);
+  }
+
+  ebac(alcohol, period) {
+    let bw = this.props.gender === "male" ? 0.58 : 0.49;
+    let result = ((0.806 * (alcohol / 10) * 1.2) / (bw * this.props.weight)) - (0.017 * period);
+    return result > 0 ? result : 0;
+  }
+
+  render() {
+    let value = this.calculateEbac();
+
+    return (
+      <div>
+        Alcohol: {value}
       </div>
     );
   }
@@ -83,7 +119,7 @@ class App extends Component {
         weight: 0
       },
       drinks: [
-        {id: 1, name: "Example drink 1", amount: 50, strength: 4.3, startTime: 1483457356, finishTime: 1483458356},
+        {key: 1, name: "Example drink 1", amount: 50, strength: 4.3, startTime: new Date(1483457356000), finishTime: new Date(1483458356000)},
       ],
       exported: false
     };
@@ -108,7 +144,7 @@ class App extends Component {
     let rows = [];
 
     this.state.drinks.forEach(function (drink) {
-      rows.push(<Drink name={drink.name} amount={drink.amount} strength={drink.strength} startTime={drink.startTime} finishTime={drink.finishTime} />)
+      rows.push(<Drink key={drink.key} name={drink.name} amount={drink.amount} strength={drink.strength} startTime={drink.startTime} finishTime={drink.finishTime} />)
     });
 
     return (
@@ -118,6 +154,7 @@ class App extends Component {
         {remember}
         <button onClick={this.check} />
         {rows}
+        <Calculator drinks={this.state.drinks} weight={this.state.basicData.weight} gender={this.state.basicData.gender} />
       </div>
     );
   }
