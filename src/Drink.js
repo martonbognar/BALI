c2833de49a90386b279550db4f5d223bfefbdb2d
marton bognar <martonbognar@sch.bme.hu>
2017-01-08 01:12:42 +0100

    Fix warnings

--- src/Drink.js
+++ src/Drink.js
@@ -21,24 +21,24 @@ class Drink extends Component {
   }
 
   intervalToText(timestamp) {
-    let diff = ((new Date).getTime() - (new Date(timestamp)).getTime()) / 1000;
+    let diff = ((new Date()).getTime() - (new Date(timestamp)).getTime()) / 1000;
 
     if (diff < 10) {
       return 'just now';
     } else if (diff < 60) {
-      return parseInt(diff) + ' seconds ago';
+      return parseInt(diff, 10) + ' seconds ago';
     } else if (diff < 120) {
       return 'a minute ago';
-    } else if (diff < 60*60) {
-      return parseInt(diff/60) + ' minutes ago';
-    } else if (diff < 60*60*2) {
+    } else if (diff < 60 * 60) {
+      return parseInt(diff / 60, 10) + ' minutes ago';
+    } else if (diff < 60 * 60 * 2) {
       return 'an hour ago';
-    } else if (diff < 60*60*24) {
-      return parseInt(diff/(60*60)) + ' hours ago';
-    } else if (diff < 2*60*60*24) {
+    } else if (diff < 60 * 60 * 24) {
+      return parseInt(diff / (60 * 60), 10) + ' hours ago';
+    } else if (diff < 2 * 60 * 60 * 24) {
       return 'a day ago';
     } else {
-      return parseInt(diff/(60*60*24)) + ' days ago';
+      return parseInt(diff / (60 * 60 * 24), 10) + ' days ago';
     }
   }
 
