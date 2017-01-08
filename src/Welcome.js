c7733f6627a53345702980dd25c98e4c4ad7fb6c
marton bognar <martonbognar@sch.bme.hu>
2017-01-08 01:35:30 +0100

    Update github link

--- src/Welcome.js
+++ src/Welcome.js
@@ -5,7 +5,7 @@ class Welcome extends Component {
     return (
       <div className='welcome-div'>
         <h1>Hi!</h1>
-        <p>Welcome to BALI, the <a href='https://github.com/martonbognar/bali' target='_blank'>open-source</a> Blood Alcohol Level Indicator.</p>
+        <p>Welcome to BALI, the <a href='https://github.com/martonbognar/bali-react' target='_blank'>open-source</a> Blood Alcohol Level Indicator.</p>
         <p>Please enter your basic information and remember to drink responsibly. Never drink and drive and don't take the estimated figures on this website too seriously.</p>
       </div>
     );
