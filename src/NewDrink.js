5e01e2a920aad20c3e0db9ea1329c3d7bbfd5aac
marton bognar <martonbognar@sch.bme.hu>
2017-01-17 15:38:27 +0100

    Move const data to separate files

--- src/NewDrink.js
+++ src/NewDrink.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import DRINKS from './data/drinkList';
 
 class NewDrink extends Component {
   constructor(props) {
@@ -18,26 +19,7 @@ class NewDrink extends Component {
 
   componentWillMount() {
     this.setState({
-      drinkList: [
-        {
-          key: "dreherbak50",
-          name: "Dreher Bak 0.5l",
-          amount: 50,
-          strength: 7.3,
-        },
-        {
-          key: "aszok50",
-          name: "Arany Aszok 0.5l",
-          amount: 50,
-          strength: 4.3,
-        },
-        {
-          key: "famous4",
-          name: "Famous Grouse 4cl",
-          amount: 4,
-          strength: 40,
-        },
-      ]
+      drinkList: DRINKS
     });
   }
 
---
+++ src/data/drinkList.js
@@ -0,0 +1,22 @@
+const DRINKS = [
+  {
+    "key": "dreherbak50",
+    "name": "Dreher Bak 0.5l",
+    "amount": 50,
+    "strength": 7.3
+  },
+  {
+    "key": "aszok50",
+    "name": "Arany Aszok 0.5l",
+    "amount": 50,
+    "strength": 4.3
+  },
+  {
+    "key": "famous4",
+    "name": "Famous Grouse 4cl",
+    "amount": 4,
+    "strength": 40
+  }
+];
+
+export default DRINKS;
