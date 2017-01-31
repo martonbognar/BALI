b6af06e0456185662c970990f4279d0647b33584
marton bognar <martonbognar@sch.bme.hu>
2017-01-31 20:13:31 +0100

    Add a few more drinks and order by name

--- src/data/drinkList.js
+++ src/data/drinkList.js
@@ -3,16 +3,28 @@ const DRINKS = [
     "categoryName": "Beers",
     "values": [
       {
+        "key": "aszok50",
+        "name": "Arany Ászok 0.5l",
+        "amount": 50,
+        "strength": 4.3,
+      },
+      {
+        "key": "dreher50",
+        "name": "Dreher 0.5l",
+        "amount": 50,
+        "strength": 5.2,
+      },
+      {
         "key": "dreherbak50",
         "name": "Dreher Bak 0.5l",
         "amount": 50,
-        "strength": 7.3
+        "strength": 7.3,
       },
       {
-        "key": "aszok50",
-        "name": "Arany Aszok 0.5l",
+        "key": "kozel50",
+        "name": "Kozel 0.5l",
         "amount": 50,
-        "strength": 4.3
+        "strength": 4,
       },
     ],
   },
@@ -23,10 +35,21 @@ const DRINKS = [
         "key": "famous4",
         "name": "Famous Grouse 4cl",
         "amount": 4,
-        "strength": 40
-      }
+        "strength": 40,
+      },
     ],
-  }
+  },
+  {
+    "categoryName": "Vodkas",
+    "values": [
+      {
+        "key": "royal4",
+        "name": "Royal 4cl",
+        "amount": 4,
+        "strength": 37.5,
+      },
+    ],
+  },
 ];
 
 export default DRINKS;
